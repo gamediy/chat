c4779a07284810f4777bdc61cdfe30b5402efb0f
page <page@lottery.com>
2019-06-15 16:35:23 +0800

    增加udp分布式

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 	"math/rand"
 	"strconv"
 	"time"
+	"net"
 )
 
 var session syncmap.Map
+var udp *net.UDPConn
+
 var upgrader = &websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
@@ -22,51 +25,142 @@ var upgrader = &websocket.Upgrader{
 }
 
 type Node struct {
-	Conn    *websocket.Conn
-	Token   string
+	Conn  *websocket.Conn
+	Token string
 
 	ReadMsg chan []byte
 }
+
 //用户发送消息
 type MessageRequest struct {
 	//1:群聊
-	Type        int `json:"type"`
+	Type        int    `json:"type"`
 	Message     string `json:"message"`
 	TargetToken string `json:"target_token"`
 	Token       string `json:"token"`
 	Name        string `json:"name"`
 }
+
 //输出消息
 type MessageResponse struct {
-	Code    int `json:"code"`
+	Code    int    `json:"code"`
 	Message string `json:"message"`
-	Name string `json:"name"`
+	Name    string `json:"name"`
 	Headimg string `json:headimg`
 	Time    string `json:"time"`
 }
 
+func createUdp(){
 
 
+
+
+
+
+	addr := net.UDPAddr{
+		IP:net.IPv4(192,168,10,7),
+		Port:2100,
+	}
+	//广播地址
+	raddr:=net.UDPAddr{
+		IP:net.IPv4(255,255,255,255),
+		Port:2100,
+	}
+	conn, e := net.DialUDP("udp", &addr, &raddr)
+	if e!=nil{
+		log.Printf(e.Error())
+		return
+	}
+	udp=conn;
+
+}
+
+//通过UDP广播消息给所有机器
+func udpBroadcast(node *Node) {
+
+	for{
+		select {
+			case msg:=<-node.ReadMsg:
+
+				udp.Write(msg)
+
+		}
+	}
+
+
+}
+//udp 收消息
+func udpReceive()  {
+	addr, e := net.ResolveUDPAddr("udp", ":2100")
+	if e!=nil{
+		log.Printf(e.Error())
+		return
+	}
+	conn, error := net.ListenUDP("udp", addr)
+	if error!=nil{
+		log.Printf(error.Error())
+		return
+	}
+	defer conn.Close()
+	for{
+		msg:=make([]byte,1024)
+		n, _, error := conn.ReadFromUDP(msg)
+		if error!=nil{
+			log.Printf(error.Error())
+
+		}else{
+			log.Printf("udp收到消息:%s",string(msg))
+			log.Println(string(msg))
+			m := MessageRequest{}
+			error:=json.Unmarshal(msg[:n], &m)
+			if error!=nil{
+				log.Printf("error ",error.Error())
+
+			}else{
+				distributionMessage(&m)
+			}
+		}
+	}
+
+}
 
 func readMessage(node *Node) {
 	for {
 		_, p, err := node.Conn.ReadMessage()
 		if err != nil {
 			session.Delete(node.Token)
-			log.Printf("token %s :下线 还有在线：%d", node.Token,*session.Length())
+			log.Printf("token %s :下线 还有在线：%d", node.Token, *session.Length())
 			return
-		} else
-		{
-			node.ReadMsg <- p
-			log.Println(p)
+		} else {
+			//如果消息格式不对，就不传递
+			go func(){
+				m := MessageRequest{}
+				err := json.Unmarshal(p, &m)
+				if err != nil {
+					errMsg := MessageResponse{
+						Message: "消息格式不对:" + err.Error(),
+						Code:    -1,
+					}
+					bytes, _ := json.Marshal(errMsg)
+					node.Conn.WriteMessage(websocket.TextMessage,bytes)
+				}else{
+					node.ReadMsg <- p
+					log.Println(p)
+				}
+
+			}()
+
 		}
 
 	}
 }
+
 //处理分发消息
 func distributionMessage(message *MessageRequest) {
 	///群聊
+	print("11:",message.Type)
 	if message.Type == 1 {
+
 		log.Print("群聊")
 		session.Range(func(key, value interface{}) bool {
 			print(key)
@@ -85,22 +179,20 @@ func distributionMessage(message *MessageRequest) {
 			}
 			return true
 		})
-	}else if(message.Type==2){//单聊
+	} else if (message.Type == 2) { //单聊
 
 	}
 }
+
 //给客户端发送消息
-func writeMessage(node *Node) {
-	for {
-		select {
-		case msg := <-node.ReadMsg:
+func writeMessage(node *Node,msg []byte) {
 			log.Println(string(msg))
 			m := MessageRequest{}
 			err := json.Unmarshal(msg, &m)
 			if err != nil {
-				errMsg:=MessageResponse{
-					Message:"消息格式不对:"+err.Error(),
-					Code:-1,
+				errMsg := MessageResponse{
+					Message: "消息格式不对:" + err.Error(),
+					Code:    -1,
 				}
 				bytes, _ := json.Marshal(errMsg)
 				node.Conn.WriteMessage(websocket.TextMessage, bytes)
@@ -108,12 +200,14 @@ func writeMessage(node *Node) {
 				distributionMessage(&m)
 			}
 
-		}
 
-	}
+
+
 }
 
 func main() {
+	go createUdp()
+	go udpReceive()
 	print(strconv.Itoa(rand.Int()))
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	http.HandleFunc("/ws", func(writer http.ResponseWriter, request *http.Request) {
@@ -126,9 +220,10 @@ func main() {
 			ReadMsg: make(chan []byte, 1024),
 		}
 		session.Store(token, node)
-		log.Printf("token %s :上线 还有在线：%d", node.Token,*session.Length())
+		log.Printf("token %s :上线 还有在线：%d", node.Token, *session.Length())
 		go readMessage(node)
-		go writeMessage(node)
+		//先通过UDP广播
+		go udpBroadcast(node)
 
 		if err != nil {
 			log.Println(err.Error())
